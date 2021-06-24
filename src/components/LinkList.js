d998400b13d30c6407776372e2c455e0cf49773a
Salvador Martinez Espinosa <84885134+salvadormartinez50@users.noreply.github.com>
2021-06-23 22:02:22 -0500

    Ajuste de LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Link from './Link';
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
+import Link from "./Link";
+import { useQuery, gql } from "@apollo/client";
 
 const FEED_QUERY = gql`
   {
@@ -17,49 +17,19 @@ const FEED_QUERY = gql`
 `;
 
 const LinkList = () => {
-   const { data } = useQuery(FEED_QUERY);
-  
-   /* return (
-      <div>
-        {data && (
-          <>
-            {data.feed.links.map((link) => (
-              <Link key={link.id} link={link} />
-            ))}
-          </>
-        )}
-      </div>
-    );
-  };
-
-export default LinkList;*/
-
-  /*const linksToRender = [
-    {
-      id: '1',
-      description:
-        'Prisma gives you a powerful database toolkit 😎',
-      url: 'https://prisma.io'
-    },
-    {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
-    }
-  ];*/
+  const { data } = useQuery(FEED_QUERY);
 
   return (
     <div>
-       {data && (
-          <>
-            {data.feed.links.map((link) => (
-              <Link key={link.id} link={link} />
-            ))}
-          </>
-        )}
+      {data && (
+        <>
+          {data.feed.links.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
 
 export default LinkList;
-
