79086ed2c61296504db150b9c3bb6035876998c2
Salvador Martinez Espinosa <84885134+salvadormartinez50@users.noreply.github.com>
2021-06-23 21:57:03 -0500

    Query

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
-import logo from './../logo.svg';
-import './../Styles/App.css';
+import LinkList from './LinkList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Primer codigo en la web, con React, 23 de junio 2021.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <LinkList />;
+  }
 }
 
 export default App;
